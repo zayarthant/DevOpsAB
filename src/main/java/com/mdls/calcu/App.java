9bcc6b38dd35b71ecd6b353a3ce1d4a57c4de22e
zayarthant.deca@gmail.com <zayarthant.deca@gmail.com>
2020-08-02 22:41:29 +0630

    modify version commit

--- src/main/java/com/mdls/calcu/App.java
+++ src/main/java/com/mdls/calcu/App.java
@@ -5,5 +5,7 @@ public class App {
         Calcu calcu = new Calcu();
         System.out.println(calcu.add(1,3));
         System.out.println(calcu.sub(10,4));
+        System.out.println(calcu.mul(3,4));
+        System.out.println("That great");
     }
 }
